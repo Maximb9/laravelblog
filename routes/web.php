563508c75806b044350f232fd8d4e713aba95422
Maxim <bmw_077@mail.ru>
2021-08-04 11:44:53 +0300

    added admin panel

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Admin\MainController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// 1. Создали админский контроллер, подключили вид.
+// 2. Посадили шаблон (админская панель). Минифицировали файлы.
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    Route::get('/', [MainController::Class, 'index'])->name('admin.index');
+});
+
+
