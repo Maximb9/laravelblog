d9b595c1e6c0145dcf589c8787805758cbb7bc0a
Maxim <bmw_077@mail.ru>
2021-08-04 15:39:52 +0300

    sluggable many to many has one belongs to

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    Use Sluggable;
+
+
+    public function tags() {
+
+        return $this->belongsToMany(Tag::Class);
+    }
+
+
+    public function category() {
+        return $this->belongsTo(Category::Class);
+    }
+
+
+    // источник title
+    public function sluggable () {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Tag extends Model
 {
-    use HasFactory;
+    Use Sluggable;
+
+
+    public function posts() {
+
+        return $this->belongsToMany(Post::Class);
+    }
+
+
+    // источник title
+    public function sluggable () {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
